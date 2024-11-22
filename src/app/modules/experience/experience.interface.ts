4df1547e1e8c768719f8d22d4f1d6680be710f31
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 15:48:12 +0600

    API: creating experience APIs interface

--- src/app/modules/experience/experience.interface.ts
+++ src/app/modules/experience/experience.interface.ts
@@ -1,1 +1,10 @@
-export interface IExperience {}
+export type TExperience = {
+  title: string;
+  company: string;
+  location: string;
+  startDate: Date;
+  endDate?: Date;
+  description: string;
+  responsibilities: string[];
+  keyInitiatives: string[];
+};
