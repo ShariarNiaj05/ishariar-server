72a64dd72434fa06b4b2457619fd149d79bd0692
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 17:52:01 +0600

    API: creating project apis

--- src/app/modules/projects/projects.model.ts
+++ src/app/modules/projects/projects.model.ts
@@ -1,7 +1,22 @@
 import mongoose, { Schema } from 'mongoose';
 import { IProjects } from './projects.interface';
 
-const projectsSchema: Schema<IProjects> = new mongoose.Schema({});
+const projectsSchema: Schema<IProjects> = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    techStack: { type: [String], required: true },
+    features: { type: [String], required: true },
+    role: { type: String, required: true },
+    challengesSolved: { type: [String], required: true },
+    liveDemo: { type: String },
+    sourceCode: { type: String },
+    mediaLinks: { type: [String] },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const ProjectsModel = mongoose.model<IProjects>(
   'Projects',
