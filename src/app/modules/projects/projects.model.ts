22722488825b08493f19e9a1165b5538594af227
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 17:03:38 +0600

    API: creating project apis

--- src/app/modules/projects/projects.model.ts
+++ src/app/modules/projects/projects.model.ts
@@ -1,18 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
-import { IExperience } from './projects.interface';
+import { IProjects } from './projects.interface';
 
-const experienceSchema: Schema<IExperience> = new mongoose.Schema({
-  title: { type: String, required: true },
-  company: { type: String, required: true },
-  location: { type: String, required: true },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date },
-  description: { type: String, required: true },
-  responsibilities: { type: [String], required: true },
-  keyInitiatives: { type: [String] },
-});
+const experienceSchema: Schema<IProjects> = new mongoose.Schema({});
 
-export const ExperienceModel = mongoose.model<IExperience>(
-  'Experience',
+export const ProjectsModel = mongoose.model<IProjects>(
+  'Projects',
   experienceSchema,
 );
