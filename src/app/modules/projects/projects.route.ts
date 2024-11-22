89c07097e6fcaa6bbae2732cc9a4b46a11767b79
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 18:11:22 +0600

    API: creating project apis

--- src/app/modules/projects/projects.route.ts
+++ src/app/modules/projects/projects.route.ts
@@ -3,6 +3,12 @@ import { ProjectsController } from './projects.controller';
 
 const router = express.Router();
 
-router.post('/add-projects', ProjectsController.createProjects);
+// router.post('/add-projects', ProjectsController.createProjects);
+
+router.get('/projects', ProjectsController.getAllProjects);
+router.get('/projects/:id', ProjectsController.getProjectById);
+router.post('/projects', ProjectsController.addProject);
+router.put('/projects/:id', ProjectsController.updateProject);
+router.delete('/projects/:id', ProjectsController.deleteProject);
 
 export const ProjectsRoutes = router;
