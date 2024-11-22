4c10dd8f814c9f31bb9e8ecc99d312c5e4cac9ca
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 15:51:32 +0600

    API: creating experience APIs route

--- src/app/modules/experience/experience.route.ts
+++ src/app/modules/experience/experience.route.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/add-experience', AuthControllers.loginUser);
+router.post('/add-experience', ExperienceController.);
 
 export const ExperienceRoutes = router;
