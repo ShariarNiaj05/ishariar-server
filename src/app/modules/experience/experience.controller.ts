ef31ad53480a4316eff321a7b290c567a7e15e49
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 15:55:02 +0600

    API: creating experience APIs route

--- src/app/modules/experience/experience.controller.ts
+++ src/app/modules/experience/experience.controller.ts
@@ -1,1 +1,26 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+
+const createExperience = catchAsync(async (req: Request, res: Response) => {
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new Error('User not authenticated');
+  }
+
+  console.log('Request Headers:', req.headers);
+  console.log('Request Body:', req.body);
+
+  const result = await ContentService.createContentIntoDB(
+    req.body,
+    req.user?._id,
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Content created successfully',
+    data: result,
+  });
+});
+
 export const ExperienceController = {};
