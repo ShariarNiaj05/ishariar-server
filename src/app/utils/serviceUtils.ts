e6fec16e62602bfbb82c6f2cdaff00ab9c862000
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 19:23:27 +0600

    Feat: file uploading feature adding

--- src/app/utils/serviceUtils.ts
+++ src/app/utils/serviceUtils.ts
@@ -1,5 +1,40 @@
+import { IUploadFile } from '../interface/file';
+import fs from 'fs';
+
 //* Split fine mimetype and get fist index
 export function getFileType(file: IUploadFile): string {
   const mimeTypeParts = file.mimetype.split('/');
   return mimeTypeParts[1] ? mimeTypeParts[1] : 'Unknown';
 }
+
+export const filePayload = (file: IUploadFile, BucketName: string) => {
+  const fileContent = fs.readFileSync(file.path);
+
+  //* R2 storage payload data
+  const Payload = {
+    Bucket: BucketName,
+    Key: `${Date.now()}_${file.originalname}`,
+    Body: fileContent,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  };
+  return Payload;
+};
+
+export const FileUpdatePayload = (
+  file: IUploadFile,
+  BucketName: string,
+  key: string,
+) => {
+  const fileContent = fs.readFileSync(file.path);
+
+  //* R2 storage payload data
+  const Payload = {
+    Bucket: BucketName,
+    Key: key,
+    Body: fileContent,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  };
+  return Payload;
+};
