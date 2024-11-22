fab6d25389b60d3fec901933f63c5db9cdad09f8
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-22 15:51:13 +0600

    API: creating experience APIs route

--- src/app/modules/demo/demo.route.ts
+++ src/app/modules/demo/demo.route.ts
@@ -2,10 +2,4 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post(
-  '/login',
-  // validateRequest(AuthValidation.loginValidationSchema),
-  AuthControllers.loginUser,
-);
-
 export const DemoRoutes = router;
