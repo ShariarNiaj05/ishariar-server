69ba055a03c7c05d75bff7240c8e3218a8a0786c
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 21:51:06 +0600

    API: SKills Apis adding

--- src/app/modules/skills/skills.controller.ts
+++ src/app/modules/skills/skills.controller.ts
@@ -1,34 +1,15 @@
-import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
-import httpStatus from 'http-status';
-import { ExperienceService } from './skills.service';
-import AppError from '../../errors/AppError';
+import { SkillsService } from './skills.service';
 
-const createExperience = catchAsync(async (req: Request, res: Response) => {
-  try {
-    const newProject = await ExperienceService.createExperienceIntoDB(req);
-
-    sendResponse(res, {
-      statusCode: httpStatus.CREATED,
-      success: true,
-      message: 'Experience created successfully',
-      data: newProject,
-    });
-  } catch (error) {
-    console.log('Request Body:', error);
-    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to add Experience');
-  }
-});
-
-const getExperiences = catchAsync(async (req: Request, res: Response) => {
-  const experiences = await ExperienceService.getAllExperiences();
+const getAllSkills = catchAsync(async (req: Request, res: Response) => {
+  const skills = await SkillsService.getSkills();
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Experiences fetched successfully',
-    data: experiences,
+    message: 'Skills retrieved successfully',
+    data: skills,
   });
 });
 
