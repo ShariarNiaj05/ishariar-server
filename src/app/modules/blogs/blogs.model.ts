07a01818303ae1ba731d6bb753f8c35cbcbe8479
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 23:23:36 +0600

    API: blogs Apis adding

--- src/app/modules/blogs/blogs.model.ts
+++ src/app/modules/blogs/blogs.model.ts
@@ -4,23 +4,33 @@ import { IBlog } from '../skills/skills.interface';
 
 const blogSchema: Schema<IBlog> = new mongoose.Schema(
   {
-    title: { type: String, required: true },
-    company: { type: String, required: true },
-    location: { type: String, required: true },
-    startDate: { type: Date, required: true },
-    endDate: { type: Date },
-    description: { type: String, required: true },
-    responsibilities: { type: [String], required: true },
-    keyInitiatives: { type: [String] },
-    media: [
+    title: { type: String, required: true }, // Blog title
+    content: { type: String, required: true }, // Blog content in HTML format
+    coverImage: {
+      url: { type: String, required: true }, // Cover image URL
+      key: { type: String, required: true }, // Cover image key
+    },
+    status: {
+      type: String,
+      enum: ['Draft', 'Published'],
+      required: true,
+    }, // Status of the blog
+    views: { type: Number, default: 0 }, // Number of views
+    likes: { type: Number, default: 0 }, // Number of likes
+    comments: [
       {
-        url: { type: String, required: true },
-        key: { type: String, required: true },
+        name: { type: String, required: true }, // Commenter's name
+        email: { type: String, required: true }, // Commenter's email
+        comment: { type: String, required: true }, // Comment text
+        date: { type: Date, required: true }, // Comment date
       },
-    ],
+    ], // Array of comments
   },
   {
     timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   },
 );
 
