ee872b5302c6b7a1eac33ab62a23ed8aac840bc1
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 22:03:01 +0600

    API: SKills Apis adding

--- src/app/modules/skills/skills.route.ts
+++ src/app/modules/skills/skills.route.ts
@@ -1,19 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { SkillsController } from './skills.controller';
+import { FileUploadConfig } from '../../config/multer';
 // import { FileUploadConfig } from '../../config/multer';
 
 const router = express.Router();
 
-// router.post(
-//   '/',
-//   FileUploadConfig.upload.fields([{ name: 'media' }]),
-//   (req: Request, res: Response, next: NextFunction) => {
-//     const parsedData = JSON.parse(req.body.data || '{}');
-//     req.body = { ...parsedData, ...req.body };
-//     req.body = JSON.parse(req.body.data);
-//     return ExperienceController.createExperience(req, res, next);
-//   },
-// );
+router.post(
+  '/',
+  FileUploadConfig.upload.fields([{ name: 'media' }]),
+  (req: Request, res: Response, next: NextFunction) => {
+    const parsedData = JSON.parse(req.body.data || '{}');
+    req.body = { ...parsedData, ...req.body };
+    req.body = JSON.parse(req.body.data);
+    return SkillsController.createSkill(req, res, next);
+  },
+);
 
 // GET: Retrieve all skills
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
