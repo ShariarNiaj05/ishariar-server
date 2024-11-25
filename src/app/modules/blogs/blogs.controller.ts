9d4a47362a715b2f1a5a4462933c62d40daf58db
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 23:27:33 +0600

    API: blogs Apis adding

--- src/app/modules/blogs/blogs.controller.ts
+++ src/app/modules/blogs/blogs.controller.ts
@@ -2,33 +2,33 @@ import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
-import { ExperienceService } from './blogs.service';
+import { BlogsService, ExperienceService } from './blogs.service';
 import AppError from '../../errors/AppError';
 
 const createBlog = catchAsync(async (req: Request, res: Response) => {
   try {
-    const newProject = await ExperienceService.createExperienceIntoDB(req);
+    const newProject = await BlogsService.createBlogsIntoDB(req);
 
     sendResponse(res, {
       statusCode: httpStatus.CREATED,
       success: true,
-      message: 'Experience created successfully',
+      message: 'Blogs created successfully',
       data: newProject,
     });
   } catch (error) {
     console.log('Request Body:', error);
-    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to add Experience');
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to add Blogs');
   }
 });
 
 const getBlogs = catchAsync(async (req: Request, res: Response) => {
-  const experiences = await ExperienceService.getAllExperiences();
+  const blogs = await BlogsService.getAllBlogs();
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Experiences fetched successfully',
-    data: experiences,
+    message: 'Blogs fetched successfully',
+    data: blogs,
   });
 });
 
