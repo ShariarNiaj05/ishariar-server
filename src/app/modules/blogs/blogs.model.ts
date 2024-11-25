d0a6ddfe7d6844f3c33cd0954a46bb9568753a52
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 23:24:19 +0600

    API: blogs Apis adding

--- src/app/modules/blogs/blogs.model.ts
+++ src/app/modules/blogs/blogs.model.ts
@@ -1,30 +1,29 @@
 import mongoose, { Schema } from 'mongoose';
-import { IExperience } from './blogs.interface';
 import { IBlog } from '../skills/skills.interface';
 
 const blogSchema: Schema<IBlog> = new mongoose.Schema(
   {
-    title: { type: String, required: true }, // Blog title
-    content: { type: String, required: true }, // Blog content in HTML format
+    title: { type: String, required: true },
+    content: { type: String, required: true },
     coverImage: {
-      url: { type: String, required: true }, // Cover image URL
-      key: { type: String, required: true }, // Cover image key
+      url: { type: String, required: true },
+      key: { type: String, required: true },
     },
     status: {
       type: String,
       enum: ['Draft', 'Published'],
       required: true,
-    }, // Status of the blog
-    views: { type: Number, default: 0 }, // Number of views
-    likes: { type: Number, default: 0 }, // Number of likes
+    },
+    views: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
     comments: [
       {
-        name: { type: String, required: true }, // Commenter's name
-        email: { type: String, required: true }, // Commenter's email
-        comment: { type: String, required: true }, // Comment text
-        date: { type: Date, required: true }, // Comment date
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        comment: { type: String, required: true },
+        date: { type: Date, required: true },
       },
-    ], // Array of comments
+    ],
   },
   {
     timestamps: true,
