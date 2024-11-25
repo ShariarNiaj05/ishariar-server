e7152c9b80c6cd63358ebe2faf51f583edcd5342
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 21:49:25 +0600

    API: SKills Apis adding

--- src/app/modules/skills/skills.route.ts
+++ src/app/modules/skills/skills.route.ts
@@ -4,17 +4,20 @@ import { FileUploadConfig } from '../../config/multer';
 
 const router = express.Router();
 
-router.post(
-  '/',
-  FileUploadConfig.upload.fields([{ name: 'media' }]),
-  (req: Request, res: Response, next: NextFunction) => {
-    const parsedData = JSON.parse(req.body.data || '{}');
-    req.body = { ...parsedData, ...req.body };
-    req.body = JSON.parse(req.body.data);
-    return ExperienceController.createExperience(req, res, next);
-  },
-);
+// router.post(
+//   '/',
+//   FileUploadConfig.upload.fields([{ name: 'media' }]),
+//   (req: Request, res: Response, next: NextFunction) => {
+//     const parsedData = JSON.parse(req.body.data || '{}');
+//     req.body = { ...parsedData, ...req.body };
+//     req.body = JSON.parse(req.body.data);
+//     return ExperienceController.createExperience(req, res, next);
+//   },
+// );
 
-router.get('/', ExperienceController.getExperiences);
+// GET: Retrieve all skills
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  return SkillController.getAllSkills(req, res, next);
+});
 
 export const ExperienceRoutes = router;
