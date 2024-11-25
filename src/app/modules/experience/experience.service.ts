d157de0c546baba4aa3dbf3f74017a80b6454cb7
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 18:35:01 +0600

    Fix: updating experiece route

--- src/app/modules/experience/experience.service.ts
+++ src/app/modules/experience/experience.service.ts
@@ -4,6 +4,10 @@ import { ExperienceModel } from './experience.model';
 const createExperienceIntoDB = async (req: Request) => {
   try {
     const newExperience = await ExperienceModel.create(req);
+
+    //@ts-expect-error: possible null error
+    const media = req.files['media']?.[0] ?? null;
+
     return newExperience;
   } catch (error) {
     console.error('Error in create  experience IntoDB:', error);
