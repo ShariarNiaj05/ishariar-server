bd02dea704d1d15b91b4c6fde8da1fc8c1640d12
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 21:51:27 +0600

    API: SKills Apis adding

--- src/app/modules/skills/skills.controller.ts
+++ src/app/modules/skills/skills.controller.ts
@@ -1,6 +1,8 @@
+import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { SkillsService } from './skills.service';
+import httpStatus from 'http-status';
 
 const getAllSkills = catchAsync(async (req: Request, res: Response) => {
   const skills = await SkillsService.getSkills();
@@ -13,4 +15,4 @@ const getAllSkills = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const ExperienceController = { createExperience, getExperiences };
+export const ExperienceController = { getAllSkills };
