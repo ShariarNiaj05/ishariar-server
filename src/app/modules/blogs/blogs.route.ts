8d39c6d3ea39061d3b13359bbda32a1559e5f20a
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 23:30:07 +0600

    API: blogs Apis adding

--- src/app/modules/blogs/blogs.route.ts
+++ src/app/modules/blogs/blogs.route.ts
@@ -1,19 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { FileUploadConfig } from '../../config/multer';
+import { BlogsController } from './blogs.controller';
 
 const router = express.Router();
 
-// router.post(
-//   '/',
-//   FileUploadConfig.upload.fields([{ name: 'media' }]),
-//   (req: Request, res: Response, next: NextFunction) => {
-//     const parsedData = JSON.parse(req.body.data || '{}');
-//     req.body = { ...parsedData, ...req.body };
-//     req.body = JSON.parse(req.body.data);
-//     return ExperienceController.createExperience(req, res, next);
-//   },
-// );
+router.post(
+  '/',
+  FileUploadConfig.upload.fields([{ name: 'coverImage' }]),
+  (req: Request, res: Response, next: NextFunction) => {
+    const parsedData = JSON.parse(req.body.data || '{}');
+    req.body = { ...parsedData, ...req.body };
+    req.body = JSON.parse(req.body.data);
+    return BlogsController.getBlogs(req, res, next);
+  },
+);
 
-// router.get('/', ExperienceController.getExperiences);
+router.get('/', BlogsController.getBlogs);
 
 export const BlogsRoutes = router;
