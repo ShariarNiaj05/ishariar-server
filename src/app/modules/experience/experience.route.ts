4a7fc5d4fdffa67a3d37ffcd449df8ce24ef9c82
ShariarNiaj05 <shariarn85@gmail.com>
2024-11-25 18:31:43 +0600

    Fix: updating experiece route

--- src/app/modules/experience/experience.route.ts
+++ src/app/modules/experience/experience.route.ts
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { ExperienceController } from './experience.controller';
+import { FileUploadConfig } from '../../config/multer';
 
 const router = express.Router();
 
-router.post('/add-experience', ExperienceController.createExperience);
+router.post(
+  '/',
+  FileUploadConfig.upload.fields([{ name: 'media' }]),
+  (req: Request, res: Response, next: NextFunction) => {
+    const parsedData = JSON.parse(req.body.data || '{}');
+    req.body = { ...parsedData, ...req.body };
+    req.body = JSON.parse(req.body.data);
+    return ExperienceController.createExperience(req, res, next);
+  },
+);
 
 export const ExperienceRoutes = router;
